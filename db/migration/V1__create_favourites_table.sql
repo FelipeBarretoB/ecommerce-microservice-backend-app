b8d41c0c05165eafbb3fe9e6d9835f08922ebb33
FelipeBarretoB <pipe.barreto07@gmail.com>
2025-06-01 14:17:04 -0500

    deleted logs and duped db, the spring profile has been set back to dev

--- db/migration/V1__create_favourites_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE favourites (
-    user_id INT NOT NULL,
-    product_id INT NOT NULL,
-    like_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NULL,
-    PRIMARY KEY (user_id, product_id, like_date)
-);
