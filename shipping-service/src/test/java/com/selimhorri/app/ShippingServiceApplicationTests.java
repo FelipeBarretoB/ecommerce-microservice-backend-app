8744065fd5716eeab687c5d17936d90fd513e145
FelipeBarretoB <pipe.barreto07@gmail.com>
2025-06-11 11:50:54 -0500

    feat: added surefire for test paralelism

--- shipping-service/src/test/java/com/selimhorri/app/ShippingServiceApplicationTests.java
+++ shipping-service/src/test/java/com/selimhorri/app/ShippingServiceApplicationTests.java
@@ -1,11 +1,17 @@
 package com.selimhorri.app;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ShippingServiceApplicationTests {
 	
-	
+	@Test
+    void testTrue() {
+        assertTrue(true);
+    }
 	
 }
 
